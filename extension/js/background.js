88ea3a761d8b42f9d5dcfd0ec2d50638329d533c
fatimaavilac <125087788+fatimaavilac@users.noreply.github.com>
2025-02-20 18:53:08 -0600

    Update background.js

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,8 +1,29 @@
 // Background Script
 // This script manages the behavior of the extension.
 
-// Event listener for browser action clicks.
-chrome.browserAction.onClicked.addListener(() => {
-    // Execute the content script file on the active tab.
-    chrome.tabs.executeScript(null, { file: 'js/content.js' });
-  });
+chrome.runtime.onInstalled.addListener(function () {
+    let defaultSettings = {
+        "local": {
+            "errors": []
+        },
+        "sync": {
+            "new_install": true,
+            "canvas_domains": [""]
+        }
+    };
+
+    chrome.storage.local.set(defaultSettings.local, () => {
+        console.log("Authentiya - Local storage initialized: ", defaultSettings.local);
+    });
+
+    chrome.storage.sync.set(defaultSettings.sync, () => {
+        console.log("Authentiya - Sync storage initialized: ", defaultSettings.sync);
+    });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ["js/content.js"]
+    }).then(() => console.log("content.js injected!"))
+    .catch(err => console.error("Injection error:", err));
+
+});
