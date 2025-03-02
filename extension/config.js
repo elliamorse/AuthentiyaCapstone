2593816e9ffce9bed4f1a3ebb6c3beaa2b47d859
v516 <71415326+v516@users.noreply.github.com>
2025-03-01 21:49:34 -0600

    Add files via upload

---
+++ extension/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDA5pLCjs64O1nW6FxG1cxEkJR9Qiv2yAI",
+    authDomain: "authentiya.firebaseapp.com",
+    projectId: "authentiya",
+    storageBucket: "authentiya.firebasestorage.app",
+    messagingSenderId: "1083502008022",
+    appId: "1:1083502008022:web:f399c8b739a7b085c0db77",
+    measurementId: "G-9EMYYPCLG1"
+};
+
+export default firebaseConfig;
